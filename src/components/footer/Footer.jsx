addbe6562d13ac1860bf0f8d0649bba5d2de0ea4
Dio Lemos <pedro.devsc@outlook.com>
2023-01-27 04:57:11 -0300

    create footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,20 @@
+import React from 'react';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
-
-const Footer = (props)=>{
-
+const Footer = () => {
     return (
-        <footer className="footer">footer</footer>
-    )
+        <div className="footer">
+            <p>Made with 💚 in Joinville, Brazil</p>
+            <div className="social-icons">
+                <a href="https://github.com/dioLemos" target="_blank" rel="noopener noreferrer">
+                    <FaGithub />
+                </a>
+                <a href="https://www.linkedin.com/in/pedro-diogenes-lemos/" target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin />
+                </a>
+            </div>
+        </div>
+    );
 }
 
-
-export default Footer
+export default Footer;
